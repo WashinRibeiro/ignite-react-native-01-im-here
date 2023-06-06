720f359c9b531dcfe9587b5af886398a95b05aeb
Washington Ribeiro <washinuchiha@gmail.com>
2023-06-06 19:11:43 -0300

    feat: learning to stylize elements

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { Text } from "react-native";
-import { Fragment } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <Fragment>
-      <Text> React </Text>
-      <Text> Native </Text>
-    </Fragment>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 48
+      }}> Nome do evento </Text>
+
+      <Text style={{
+        color: '#6b6b6b',
+        fontSize: 16
+      }}> Sexta, 4 de Novembro de 2022 </Text>
+    </View>
   );
 }
